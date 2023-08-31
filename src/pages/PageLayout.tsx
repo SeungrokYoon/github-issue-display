5f758a206a60d71d24e7a7d285a56c0d629fc6fe
Seungrok <sonicrok93@gmail.com>
2023-08-31 23:17:13 +0900

    refactor: move Header directory

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from '../components/IssueList/Header'
+import Header from '../components/Header'
 import styled from 'styled-components'
 
 function PageLayout() {
